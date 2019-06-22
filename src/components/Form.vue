6baaa8c4bedf3a62d94d6bcfcfb2146dcf26a7c4
washoe <b.oliver@optusnet.com.au>
2019-06-22 13:43:54 +1000

    Add more fields

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -12,17 +12,53 @@
       :aria-invalid="messages.requiredText">
       <p class="messages" v-if="messages.requiredText">There is an error</p>
     </div>
-    <!-- max and min length -->
+    <!-- max and min length. Here we use pattern validation, as minlength/maxlength will constrain the input. -->
     <div class="field">
       <label for="minmaxText">This text must be between 2-8 characters</label>
       <input type="text"
       name="minmaxText"
       id="minmaxText"
-      minlength="2"
-      maxlength="8"
+      pattern="^(.{2,8})$"
       @blur="handleBlur"
       :aria-invalid="messages.minmaxText">
       <p class="messages" v-if="messages.minmaxText">There is an error</p>
+    </div>
+        <!-- required + max and min length. Combination of the above -->
+    <div class="field">
+      <label for="requiredMinmaxText">This text is required and must also be between 2-8 characters</label>
+      <input type="text"
+      name="requiredMinmaxText"
+      id="requiredMinmaxText"
+      pattern="^(.{2,8})$"
+      required
+      @blur="handleBlur"
+      :aria-invalid="messages.requiredMinmaxText">
+      <p class="messages" v-if="messages.requiredMinmaxText">There is an error</p>
+    </div>
+    <!-- max and min number. -->
+    <div class="field">
+      <label for="minmaxNumber">This number must be between 2-8</label>
+      <input type="number"
+      name="minmaxNumber"
+      id="minmaxNumber"
+      pattern="^[0-9]$"
+      min="2"
+      max="8"
+      @blur="handleBlur"
+      :aria-invalid="messages.minmaxNumber">
+      <p class="messages" v-if="messages.minmaxNumber">There is an error</p>
+    </div>
+    <!-- date - must not be in the past. We need to set a sensible-ish max as well. Without this, using the down arrow on the year field will give 275760 (on chrome)-->
+    <div class="field">
+      <label for="futureDate">This date must be today or later</label>
+      <input type="date"
+      name="futureDate"
+      id="futureDate"
+      :min="today"
+      mmax="2099-12-31"
+      @blur="handleBlur"
+      :aria-invalid="messages.futureDate">
+      <p class="messages" v-if="messages.futureDate">There is an error</p>
     </div>
   </fieldset>
   <button type="submit">Submit</button>
@@ -44,7 +80,8 @@ export default {
   name: 'Form',
   data: ()=> {
     return {
-      messages: {}
+      messages: {},
+      today: new Date().toISOString().slice(0, 10)
     };
   },
   methods: {
