7b4d3c352a9d5359cd1308abdd646aebf0e15e9a
washoe <b.oliver@optusnet.com.au>
2019-06-29 16:56:56 +1000

    Support custom messages
    Map native validation state to custom strings
    Render messages by updating reportedMessages object
    Support multiple messages

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,5 +1,5 @@
 <template>
-  <form novalidate @submit.prevent="handleSubmit">
+  <form novalidate @submit.prevent="handleSubmit" @reset="handleReset">
     <fieldset>
       <!-- required -->
       <div class="field">
@@ -9,10 +9,10 @@
           id="requiredText"
           required
           @blur="handleBlur"
-          :aria-invalid="messages.requiredText"
+          :aria-invalid="reportedMessages.requiredText"
         >
         <label for="requiredText">This text is required</label>
-        <p class="messages" v-if="messages.requiredText">There is an error</p>
+        <p class="messages" v-if="reportedMessages.requiredText">{{reportedMessages.requiredText}}</p>
       </div>
       <!-- max and min length. Here we use pattern validation, as minlength/maxlength will constrain the input. -->
       <div class="field">
@@ -22,10 +22,10 @@
           id="minmaxText"
           pattern="^(.{2,8})$"
           @blur="handleBlur"
-          :aria-invalid="messages.minmaxText"
+          :aria-invalid="reportedMessages.minmaxText"
         >
         <label for="minmaxText">This text must be between 2-8 characters</label>
-        <p class="messages" v-if="messages.minmaxText">There is an error</p>
+        <p class="messages" v-if="reportedMessages.minmaxText">{{reportedMessages.minmaxText}}</p>
       </div>
       <!-- required + max and min length. Combination of the above -->
       <div class="field">
@@ -36,12 +36,15 @@
           pattern="^(.{2,8})$"
           required
           @blur="handleBlur"
-          :aria-invalid="messages.requiredMinmaxText"
+          :aria-invalid="reportedMessages.requiredMinmaxText"
         >
         <label
           for="requiredMinmaxText"
         >This text is required and must also be between 2-8 characters</label>
-        <p class="messages" v-if="messages.requiredMinmaxText">There is an error</p>
+        <p
+          class="messages"
+          v-if="reportedMessages.requiredMinmaxText"
+        >{{reportedMessages.requiredMinmaxText}}</p>
       </div>
       <!-- max and min number. -->
       <div class="field">
@@ -53,10 +56,10 @@
           min="2"
           max="8"
           @blur="handleBlur"
-          :aria-invalid="messages.minmaxNumber"
+          :aria-invalid="reportedMessages.minmaxNumber"
         >
         <label for="minmaxNumber">This number must be between 2-8</label>
-        <p class="messages" v-if="messages.minmaxNumber">There is an error</p>
+        <p class="messages" v-if="reportedMessages.minmaxNumber">{{reportedMessages.minmaxNumber}}</p>
       </div>
       <!-- date - must not be in the past. We need to set a sensible-ish max as well. Without this, using the down arrow on the year field will give 275760 (on chrome)-->
       <div class="field">
@@ -67,16 +70,38 @@
           :min="today"
           max="2099-12-31"
           @blur="handleBlur"
-          :aria-invalid="messages.futureDate"
+          :aria-invalid="reportedMessages.futureDate"
         >
-        <label for="futureDate">This date must be today or later</label>
-        <p class="messages" v-if="messages.futureDate">There is an error</p>
+        <label for="futureDate">This date must be today or later, but is not required</label>
+        <p class="messages" v-if="reportedMessages.futureDate">{{reportedMessages.futureDate}}</p>
+      </div>
+      <div class="field">
+        <input
+          type="date"
+          name="requiredDate"
+          id="requiredDate"
+          required
+          @blur="handleBlur"
+          :aria-invalid="reportedMessages.requiredDate"
+        >
+        <label for="requiredDate">This date is required</label>
+        <p class="messages" v-if="reportedMessages.requiredDate">{{reportedMessages.requiredDate}}</p>
+      </div>
+      <div class="field">
+        <input
+          type="time"
+          name="requiredTime"
+          id="requiredTime"
+          required
+          @blur="handleBlur"
+          :aria-invalid="reportedMessages.requiredTime"
+        >
+        <label for="requiredTime">This time is required</label>
+        <p class="messages" v-if="reportedMessages.requiredTime">{{reportedMessages.requiredTime}}</p>
       </div>
       <!-- required + max and min length. This time in a textarea -->
-      <!--
-The textarea appears not to support the pattern attribute in Chrome, so we cannot directly use that approach to validating length.
-
-However we can use the setCustomValidity method, which we will call on change. See https://developer.mozilla.org/en-US/docs/Web/API/Constraint_validation
+      <!-- TODO
+The textarea appears not to support the pattern attribute in Chrome, so we cannot directly use that approach to validating length. We will need to do something more complex.
       -->
       <div class="field">
         <textarea
@@ -87,10 +112,13 @@ However we can use the setCustomValidity method, which we will call on change. S
           required
           @blur="handleBlur"
           @change="handleChange"
-          :aria-invalid="messages.requiredMinmaxTextArea"
+          :aria-invalid="reportedMessages.requiredMinmaxTextArea"
         ></textarea>
         <label for="requiredMinmaxText">This text is required and has max 10 characters</label>
-        <p class="messages" v-if="messages.requiredMinmaxTextArea">There is an error</p>
+        <p
+          class="messages"
+          v-if="reportedMessages.requiredMinmaxTextArea"
+        >{{reportedMessages.requiredMinmaxTextArea}}</p>
       </div>
       <!-- required select -->
       <div class="field">
@@ -99,19 +127,25 @@ However we can use the setCustomValidity method, which we will call on change. S
           id="requiredSelect"
           required
           @blur="handleBlur"
-          :aria-invalid="messages.requiredSelect"
+          :aria-invalid="reportedMessages.requiredSelect"
         >
           <option></option>
           <option value="1">Option 1</option>
           <option value="2">Option 2</option>
         </select>
         <label for="requiredSelect">This select is required</label>
-        <p class="messages" v-if="messages.requiredSelect">There is an error</p>
+        <p
+          class="messages"
+          v-if="reportedMessages.requiredSelect"
+        >{{reportedMessages.requiredSelect}}</p>
       </div>
     </fieldset>
-    <button type="submit">Submit</button>
-    <p v-if="formIsValid">No errors reported</p>
-    <p class="messages" v-if="!formIsValid">This form has some errors</p>
+    <fieldset>
+      <button type="submit">Submit</button>
+      <button type="reset">Reset</button>
+      <p v-if="formIsValid">No errors reported</p>
+      <p class="messages form-invalid" v-if="!formIsValid">This form has some errors</p>
+    </fieldset>
   </form>
 </template>
 
@@ -121,56 +155,80 @@ However we can use the setCustomValidity method, which we will call on change. S
  * - No validation errors shown initially
  * - Report a field's validity on blur
  * - Report all fields' validity on submit
+ * - Show multiple messages simultaneously
  * - Do not prevent user from entering invalid data (no constraint)
+ * - Replace default validation messages with custom messages
  * - Form submission will be blocked if any fields are invalid
  * - All error messages to be displayed below the field
  * ref: https://www.w3.org/WAI/WCAG21/working-examples/aria-invalid-data-format/
  */
+
+/**
+ * Creates validation message for each element
+ * We use the underlying ValidationState as keys. This allows us to let the browser to do the validation where it can.
+ * We can also combine messages if a field has multiple validation errors
+ */
+const setValidationMessages = elements => {
+  // For now, the same map will be applied everywhere
+  const messageMap = {
+    badInput: "Please enter a valid value",
+    patternMismatch: "Does not match regex pattern",
+    rangeOverflow: "Too high",
+    rangeUnderflow: "Too low",
+    stepMismatch: "Does not resolve to the given step size",
+    tooLong: "Too long",
+    tooShort: "Too short",
+    typeMismatch: "Cannot be resolved to the required type",
+    valueMissing: "Cannot be blank"
+  };
+  return elements.map(element => {
+    const validationMessage = Object.entries(messageMap)
+      .filter(entry => element.validity[entry[0]])
+      .map(entry => entry[1]);
+    element.setCustomValidity(validationMessage.join("|"));
+    return element;
+  });
+};
+
 export default {
   name: "Form",
   data: () => {
     return {
-      messages: {},
-      today: new Date().toISOString().slice(0, 10),
-      formIsValid: true
+      reportedMessages: {},
+      today: new Date().toISOString().slice(0, 10)
     };
   },
+  computed: {
+    formIsValid: function() {
+      return !Object.values(this.reportedMessages).length;
+    },
+    fieldsToValidate: function() {
+      return this.$el && typeof this.$el.querySelectorAll === "function"
+        ? [...this.$el.querySelectorAll("[name]")].filter(
+            element => element.willValidate
+          )
+        : [];
+    }
+  },
   methods: {
-    setFormIsValid: function() {
-      this.$nextTick(() => {
-        this.formIsValid = !document.querySelectorAll("form [aria-invalid]")
-          .length;
-      });
-    },
-    customValidate: function(element) {
-      const patternValid =
-        !element.attributes.pattern ||
-        new RegExp(element.attributes.pattern.value).test(element.value); // true if valid
-      element.setCustomValidity(patternValid ? "" : "Too damn long!");
-    },
-    handleChange: function(event) {
-      this.customValidate(event.target);
-    },
-    runValidation: function(elements) {
-      this.messages = (elements || []).reduce((result, element) => {
-        return {
-          ...result,
-          [element.name]: !element.checkValidity() ? element.validity : null
-        };
-      }, this.messages);
-      this.setFormIsValid();
-    },
-    handleBlur: function(event) {
-      this.runValidation([event.target]);
-    },
-    handleSubmit: function(event) {
-      [...event.target.querySelectorAll("textarea")].forEach(
-        this.customValidate
+    reportMessages: function(elements) {
+      this.reportedMessages = setValidationMessages(elements).reduce(
+        (result, element) => {
+          result[element.name] = element.validationMessage ? element.validationMessage.split('|') : undefined;
+          return result;
+        },
+        { ...this.reportedMessages }
       );
-      const formInputElements = [
-        ...event.target.querySelectorAll("input, textarea, select")
-      ];
-      this.runValidation(formInputElements);
+    },
+    handleChange: function(event) {},
+    handleBlur: function(event) {
+      this.reportMessages([event.target]);
+    },
+    handleSubmit: function() {
+      this.reportMessages(this.fieldsToValidate);
+    },
+    handleReset: function() {
+      this.reportedMessages = {};
     }
   }
 };
@@ -203,7 +261,8 @@ export default {
   color: red;
 }
 
-.field :invalid ~ .messages {
+.field :invalid ~ .messages,
+.messages.form-invalid {
   color: red;
 }
 </style>
